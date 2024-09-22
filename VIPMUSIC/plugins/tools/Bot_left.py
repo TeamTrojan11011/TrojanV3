740eeb735362c99e21f27b87c2cfe8723458dda9
TeamTrojan11011 <TeamTrojans11011@gmail.com>
2024-09-23 00:08:23 +0530

    Update Bot_left.py

--- VIPMUSIC/plugins/tools/Bot_left.py
+++ VIPMUSIC/plugins/tools/Bot_left.py
@@ -24,14 +24,14 @@ async def on_left_chat_member(_, message: Message):
         left_chat_member = message.left_chat_member
         if left_chat_member and left_chat_member.id == (await app.get_me()).id:
             remove_by = (
-                message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+                message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
             )
             title = message.chat.title
             username = (
-                f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+                f"@{message.chat.username}" if message.chat.username else "ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
             )
             chat_id = message.chat.id
-            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
+            left = f"👾 <b><u>#ʟᴇғᴛ_ɢʀᴏᴜᴘ </u></b> 👾\n\nᴄʜᴀᴛ ɴᴀᴍᴇ : {title}\n\nᴄʜᴀᴛ ɪᴅ: {chat_id}\n\nʀᴇᴍᴏᴠᴇᴅ ʙʏ : {remove_by}\n\nʙᴏᴛ : @{app.username}"
             await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
             await delete_served_chat(chat_id)
             await userbot.leave_chat(chat_id)
